caccea6b1a508d11390f61e38dfe14f83e5a36a3
yupix <yupi0982@outlook.jp>
2021-08-11 18:45:19 +0900

    fix: __init__.pyに関数登録し忘れてた

--- alfakana/__init__.py
+++ alfakana/__init__.py
@@ -1,4 +1,4 @@
-from .main import sentence_kana, change_kana
+from .main import sentence_kana, change_kana, add_dic
 from .exceptions import NoSuchTable
 __copyright__ = 'Copyright (C) 2021 yupix'
 __version__ = '0.0.1'
@@ -6,4 +6,4 @@ __license__ = 'Mozilla Public License 2.0 (MPL 2.0)'
 __author__ = 'yupix'
 __author_email__ = 'yupi0982@outlook.jp'
 __url__ = 'https://github.com/yupix/alfakana'
-__all__ = ['sentence_kana', 'change_kana', 'NoSuchTable']
+__all__ = ['sentence_kana', 'change_kana', 'NoSuchTable', 'add_dic']
